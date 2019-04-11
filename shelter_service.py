de085312b6d8cb312967f42d4e784debc709ff56
Zerun <tian.ze@husky.neu.edu>
2019-04-10 20:07:19 -0400

    version 0.9

--- shelter_service.py
+++ shelter_service.py
@@ -6,9 +6,11 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def landing_page():
-    return render_template("index.html")
+    return render_template("animal_searches.html")
+    # return render_template("index.html")
 
 
 @app.route('/shelters')
@@ -17,27 +19,56 @@ def get_shelters():
         conn = mysql.connector.connect(user='root', password='cs3200db2019',
                                        host='localhost',
                                        database='animal_shelter')
-        cursor = conn.cursor()
         select_all_shelters_q = ("""select * from shelter;""")
         cursor = conn.cursor()
         cursor.execute(select_all_shelters_q)
         rows = cursor.fetchall()
-        all_data = dict()
         shelters = []
-        all_data["shelters"] = shelters
         for (id, name, address, city, state, postcode, email, phone) in rows:
             data = dict()
             data["id"] = id
             data["name"] = name
+            data["address"] = address + ", " + city + ", " + state + ". " + postcode
             shelters.append(data)
-
+        cursor.close()
+        conn.close()
         return json.dumps(shelters)
     except mysql.connector.Error as e:
         print(e)
-    finally:
+
+
+@app.route('/animals')
+def get_animals():
+    try:
+        conn = mysql.connector.connect(user='root', password='cs3200db2019',
+                                       host='localhost',
+                                       database='animal_shelter')
+        cursor = conn.cursor()
+        select_50_animals = ("""select animal_name, gender, age, size, animal_image 
+        from animal 
+        right join image using (animal_id)
+        order by animal_name limit 50;""")
+        cursor = conn.cursor()
+        cursor.execute(select_50_animals)
+        rows = cursor.fetchall()
+        animals = []
+        for (name, gender, age, size, img_url) in rows:
+            animal = dict()
+            animal['name'] = name
+            animal['gender'] = gender
+            animal['age'] = age
+            animal['size'] = size
+            animal['img_url'] = img_url
+            animals.append(animal)
         cursor.close()
         conn.close()
+        return json.dumps(animals)
+    except mysql.connector.Error as e:
+        print(e)
 
+@app.route('/animals-page')
+def get_animals_page():
+    return render_template("animals.html")
 
 
 if __name__ == '__main__':
