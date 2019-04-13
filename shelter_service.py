90d46223bb05129d948344c7283c34684515bfec
Zerun <tian.ze@husky.neu.edu>
2019-04-13 01:35:45 -0400

    version 1.0

--- shelter_service.py
+++ shelter_service.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 import mysql.connector
 import json
@@ -40,16 +40,40 @@ def get_shelters():
 @app.route('/animals')
 def get_animals():
     try:
+        type_name = request.args.get('type', default=None, type=str)
+        breed_name = request.args.get('breed', default=None, type=str)
+        size = request.args.get('size', default=None, type=str)
+        gender = request.args.get('gender', default=None, type=str)
+        age = request.args.get('age', default=None, type=str)
+
+        select_animals_by_filter = ("""select animal_name, gender, age, size, animal_image 
+        from animal
+        join breed using (breed_id)
+        join animal_type using (type_id)
+        join image using (animal_id) """)
+
+        filters = [("type_name", type_name),
+                   ("breed_name", breed_name),
+                   ("size", size),
+                   ("gender", gender),
+                   ("age", age)]
+
+        criteria = ""
+        for filter_name, filter_value in filters:
+            if filter_value is not None:
+                criteria += filter_name + " = " + "\'" + filter_value + "\'" + " and "
+
+        if criteria != "":
+            select_animals_by_filter += "\nwhere " + criteria[:-4]
+
+        select_animals_by_filter += "\norder by animal_name limit 50;"
+
+        print(select_animals_by_filter)
         conn = mysql.connector.connect(user='root', password='cs3200db2019',
                                        host='localhost',
                                        database='animal_shelter')
         cursor = conn.cursor()
-        select_50_animals = ("""select animal_name, gender, age, size, animal_image 
-        from animal 
-        right join image using (animal_id)
-        order by animal_name limit 50;""")
-        cursor = conn.cursor()
-        cursor.execute(select_50_animals)
+        cursor.execute(select_animals_by_filter)
         rows = cursor.fetchall()
         animals = []
         for (name, gender, age, size, img_url) in rows:
@@ -65,6 +89,7 @@ def get_animals():
         return json.dumps(animals)
     except mysql.connector.Error as e:
         print(e)
+
 
 @app.route('/animals-page')
 def get_animals_page():
